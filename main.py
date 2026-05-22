27fcc46f84258fda2699cfd4543ca034ca8c7e65
Jason Madar <env3d@users.noreply.github.com>
2026-05-22 22:20:27 +0000

    update build

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from chat import chat
 
-for y in range(5):
-    print("### Temperature "+str(float(y)))
-    for i in range(5):
-        print("Response "+str(i)+": "+chat("What is your purpose", float(y)))
+print(chat("What is your purpose?", 1.0))
 
